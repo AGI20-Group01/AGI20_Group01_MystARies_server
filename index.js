431dc140a275dbe2713814fcf75d655aa117a6b0
Emil Clemedson <emilcle@gmail.com>
2020-10-04 14:01:03 +0200

    added move, rotate, add and remove event

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const port = (process.env.PORT || 5000)
 const io = require('socket.io')(port);
 const shortId = require('shortid');
+const os = require("os");
 
-
+let networkInterfaces = os.networkInterfaces( );
+let address = networkInterfaces['Wi-Fi'][1].address
 const User = require('./src/User.js');
 
 const users = {}
 
-console.log(`Server started on port ${port}`)
+console.log(`Server started on ${address}:${port}`)
 
 io.on('connection', function(socket) {
     const userId = createUser();
     console.log(`user ${userId} conected to server`);
     socket.emit('userId',  {id: userId})
 
-    socket.on('avatarSpawn' , function(data) {
-        socket.broadcast.emit('avatarSpawn', data)
+    socket.on('AddCube' , function(data) {
+        //console.dir(data)
+        socket.broadcast.emit('AddCube', data)
     })
 
-    socket.on('avatarMove', function(data) {
-        socket.broadcast.emit('avatarMove', data)
+    socket.on('RemoveCube', function(data) {
+        //console.dir(data)
+        socket.broadcast.emit('RemoveCube', data)
     })
 
-    socket.on('itemMove', function(data) {
-        socket.broadcast.emit('itemMove', data)
+    socket.on('MoveTraveler', function(data) {
+        //console.dir(data)
+        socket.broadcast.emit('MoveTraveler', data)
     })
 
-    socket.on('takeMove', function(data) {
-        socket.broadcast.emit('takeMove', data)
+    socket.on('RotateTraveler', function(data) {
+        //console.dir(data)
+        socket.broadcast.emit('RotateTraveler', data)
     })
 
     socket.on('disconnect', function() {
         socket.broadcast.emit('remove', {id: userId})
         delete users[userId];
-        console.log(`user ${userId} disconnect`)
+        console.log(`user ${userId} disconnect`);
     })
 });
 
